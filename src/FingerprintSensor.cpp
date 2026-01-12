8a71e8d89bc3f3ab8e94ee2a2381afd6fa1bae97
Janjre <136253210+Janjre@users.noreply.github.com>
2026-01-12 18:26:37 +0000

    Unbroke enroll user again and added even more Debug prints

--- src/FingerprintSensor.cpp
+++ src/FingerprintSensor.cpp
@@ -15,6 +15,7 @@ FingerprintSensor::FingerprintSensor(
     Baud = baud;
     SensorSerial = &sr;
 
+
     pinMode(WAKE, INPUT);
     pinMode(RST, OUTPUT);
 }
@@ -41,42 +42,107 @@ void FingerprintSensor::shiftBuffer(byte buffer[], uint16_t bufferSize) {
 void FingerprintSensor::txCmd(byte commands[]) {
     clearTxBuffer();
     _cmdSendBuffer[0] = WVFP_CMD_HEAD;
+    for (uint8_t i = 0; i<4; i++) {
+        _cmdSendBuffer[1+i] = commands[i];
+    }
     _cmdSendBuffer[5] = 0;
 
-    for (uint8_t i = 0; i < 4; i++) {
-        _cmdSendBuffer[1 + i] = commands[i];
-    }
-
     byte checksum = 0;
-    for (uint8_t i = 1; i < 6; i++) {
-        checksum ^= _cmdSendBuffer[i];
+    for (uint8_t i = 1; i<6; i++) {
+        checksum = checksum ^ _cmdSendBuffer[i];
     }
-
     _cmdSendBuffer[6] = checksum;
     _cmdSendBuffer[7] = WVFP_CMD_TAIL;
+
+    if (DebugPrints) {
+        Serial.println("About to sent: ");
+        Serial.print(String(_cmdSendBuffer[0],HEX) + ",");
+        Serial.print(String(_cmdSendBuffer[1],HEX) + ",");
+        Serial.print(String(_cmdSendBuffer[2],HEX) + ",");
+        Serial.print(String(_cmdSendBuffer[3],HEX) + ",");
+        Serial.print(String(_cmdSendBuffer[4],HEX) + ",");
+        Serial.print(String(_cmdSendBuffer[5],HEX) + ",");
+        Serial.print(String(_cmdSendBuffer[6],HEX) + ",");
+        Serial.print(String(_cmdSendBuffer[7],HEX) + ",");
+    }
 
     SensorSerial->write(_cmdSendBuffer, WVFP_TXRXBUFFER_SIZE);
 }
 
 bool FingerprintSensor::rxCmd(byte response[], byte command) {
+    if (DebugPrints) {
+        Serial.println("START OF RX AND TX, fairly well printed");
+    }
+
+    int cnt = 0;
+    bool success = false;
     while (SensorSerial->available()) {
         shiftBuffer(_cmdReceiverBuffer, WVFP_TXRXBUFFER_SIZE);
         _cmdReceiverBuffer[WVFP_TXRXBUFFER_SIZE - 1] = SensorSerial->read();
+        cnt++;
 
-        if (_cmdReceiverBuffer[0] == WVFP_CMD_HEAD &&
-            _cmdReceiverBuffer[WVFP_TXRXBUFFER_SIZE - 1] == WVFP_CMD_TAIL) {
 
-            byte checksum = 0;
-            for (uint8_t i = 1; i < 6; i++) {
-                checksum ^= _cmdReceiverBuffer[i];
+
+        // stop reading if there is a correct frame
+        if (_cmdReceiverBuffer[0] == WVFP_CMD_HEAD && _cmdReceiverBuffer[WVFP_TXRXBUFFER_SIZE - 1] == WVFP_CMD_TAIL){// && _cmdReceiverBuffer[1] == command) {
+
+            if (DebugPrints) {
+                Serial.print("Command received");
             }
 
-            if (checksum == _cmdReceiverBuffer[6]) {
-                memcpy(response, &_cmdReceiverBuffer[1], WVFP_TXRXDATA_SIZE);
-                return true;
-            }
+            success=true;
+            break;
         }
     }
+
+    if (DebugPrints) {
+        Serial.println("Command thing recieved:");
+        Serial.print(String(_cmdReceiverBuffer[0],HEX) + ",");
+        Serial.print(String(_cmdReceiverBuffer[1],HEX) + ",");
+        Serial.print(String(_cmdReceiverBuffer[2],HEX) + ",");
+        Serial.print(String(_cmdReceiverBuffer[3],HEX) + ",");
+        Serial.print(String(_cmdReceiverBuffer[4],HEX) + ",");
+        Serial.print(String(_cmdReceiverBuffer[5],HEX) + ",");
+        Serial.print(String(_cmdReceiverBuffer[6],HEX) + ",");
+        Serial.println(String(_cmdReceiverBuffer[7],HEX) );
+    }
+
+    if (success) {
+        Serial.println("It does!");
+        byte checksum = 0;
+        for (uint8_t i = 1; i<6; i++) {
+            checksum ^= _cmdReceiverBuffer[i];
+        }
+
+        // check if checksum is correct
+        if (checksum == _cmdReceiverBuffer[WVFP_TXRXBUFFER_SIZE - 2]) {
+            if (DebugPrints) {
+                Serial.println("Checksum is right:");
+            }
+
+            if (DebugPrints) {
+                Serial.println("Printing what we are returning: ");
+            }
+
+            for (uint8_t i = 0; i < WVFP_TXRXDATA_SIZE; i++) {
+                if (DebugPrints) {
+                    Serial.print(String(_cmdReceiverBuffer[i+1],HEX) + ", ");
+                }
+                response[i] = _cmdReceiverBuffer[i+1]; // CHANGED THIS HERE
+            }
+            if (DebugPrints) Serial.println("EXITING RX successfully!");
+            return true;
+        } else {
+            Serial.println("Invalid checksum");
+        }
+    }else {
+        if (DebugPrints) {
+            Serial.println("Command is wrong!");
+        }
+    }
+
+
+    if (DebugPrints) Serial.println("EXITING RX unsuccessfully!");
     return false;
 }
 
@@ -114,20 +180,28 @@ unsigned long FingerprintSensor::getModelSN() {
 }
 
 bool FingerprintSensor::enrollUser (uint16_t id,byte permission) {
-
+    Serial.println("You are trying to enroll a user! ");
     for (int step = 1; step <= 3; step++) {
         if (DebugPrints) {
             Serial.println("Step "+String(step));
         }
 
         byte response[WVFP_TXRXBUFFER_SIZE-1];
+
         byte command[] = {(byte)step, 0, (byte)(id & 0xff00 >> 8),(byte)(id & 0x00ff),permission};
 
         if (txAndRxCmd(command,response,1000)) {
-            switch (response[4]) {
+            if (DebugPrints) {
+                Serial.print("Response: ");
+                Serial.print(String(response[0],HEX) + ", ");
+                Serial.print(String(response[1],HEX) + ", ");
+                Serial.print(String(response[2],HEX) + ", ");
+                Serial.println(String(response[3],HEX) + ", ");
+            }
+            switch (response[3]) {
                 case WVFP_ACK_SUCCESS:
                     if (DebugPrints) {
-                        Serial.println("Success");
+                        Serial.println("Success on this step");
                     }
                     break;
                 case WVFP_ACK_FULL:
@@ -142,6 +216,8 @@ bool FingerprintSensor::enrollUser (uint16_t id,byte permission) {
                 case WVFP_ACK_FINGER_OCCUPIED:
                     Serial.println("Already registered finger");
                     return false;
+                default:
+                    Serial.println("Here for some reason?");
 
             }
 
@@ -151,6 +227,8 @@ bool FingerprintSensor::enrollUser (uint16_t id,byte permission) {
         }
     }
 
+    Serial.println("Got to the end!");
 
-    return false;
+
+    return true;
 }
